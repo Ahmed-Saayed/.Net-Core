d257c2489ccfc3ead1e143fb90fb1605c3476e45
PATO <ahmed9pato7@gmail.com>
2025-01-08 21:05:00 +0200

    DONE

---
+++ C#Learn/Fundamental & Basics/Basics.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Learn.Fundamental___Basics
+{
+    public class Basics
+    {
+      
+        public void DO()
+        {
+
+            /*
+           Copy
+           int num = 42;
+           object obj = num; // Boxing
+           int num2 = (int)obj; // Unboxing 
+           */
+
+            /*
+             string name=Console.ReadLine();
+
+             Console.WriteLine($"My name {name}");
+             Console.WriteLine(name.Contains("12"));
+             Console.WriteLine(name.ToUpper());
+             Console.WriteLine(name.StartsWith('a'));
+             Console.WriteLine(name.StartsWith("a",StringComparison.OrdinalIgnoreCase));
+             Console.WriteLine(name.IndexOf("a"));
+             Console.WriteLine(name.LastIndexOf("a"));
+             Console.WriteLine(name.Replace("pato","ahmed"));
+             name = name.ToLower();
+             Console.WriteLine(name);
+             Console.WriteLine("================================");
+      
+            
+            string s = "123{0}567{1}";
+            s = string.Format(s, "but in first id", "but in second id");
+            Console.WriteLine(s);
+  
+            s=Console.ReadLine();
+            s = s.Trim();                   // remove leading and trailling spaces
+            Console.WriteLine(s);
+            var sub=s.Substring(2, 3);
+            Console.WriteLine(sub);
+            */
+
+            /*
+               public  const int n=61;              
+               public readonly int ho = 60;         
+            */
+            //readonly can nit assign when i created and assign it once in constructor But const assign when created only
+
+             
+       
+
+
+            /*
+            Random rnd = new Random();
+            int num = rnd.Next(1, 1000);
+            Console.WriteLine(num);
+            */
+
+            /*
+            string[] s = {"ab", "cd","ef"};
+            string t = string.Join(" , ", s);
+            Console.WriteLine(t);
+            */
+
+
+            /*
+            int x = int.Parse(Console.ReadLine());
+            string s=x.ToString();
+            int b=int.Parse(s);         //to int
+
+            Console.WriteLine(x + 5);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);            
+            Console.WriteLine(dfs(x));
+            Console.WriteLine(--x);
+            */
+
+
+            // Array
+            /*
+            int []x = new int[3],y=new int[3];
+            x[0] = 1;
+            x[1] = 2;
+            Console.WriteLine(x.Sum());
+            Array.Copy(x, y, x.Length);
+
+            long[] x = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();  // cin one line
+            */
+
+        }
+}
+}
+  
---
+++ C#Learn/Map/Maptest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Learn.Map
+{
+    public class Maptest
+    {
+         public void DO() { 
+            Dictionary<string, int> mp = new Dictionary<string, int>();
+
+            mp.Add("Alice", 30);
+            mp.Add("Bob", 25);
+            mp.Add("Charlie", 35);
+
+            Console.WriteLine("Alice's age: " + mp["Alice"]);
+
+           
+            if (mp.ContainsKey("Bob"))
+            {
+                Console.WriteLine("Bob's age: " + mp["Bob"]);
+            }
+
+            if (mp.TryGetValue("Charlie", out int charlieAge))
+            {
+                Console.WriteLine("Charlie's age: " + charlieAge);
+            }
+            
+            bool ok = mp.TryGetValue("hehe", out int val);
+            Console.WriteLine(val);
+
+            mp.Remove("Alice");
+
+            foreach (var kvp in mp)
+            {
+                Console.WriteLine($"{kvp.Key} is {kvp.Value} years old.");
+            }
+
+            //Console.WriteLine(mp["a"]);           runtime error u must check its in map or not
+            
+       
+            mp.Clear();
+            Console.WriteLine("Count after clearing: " + mp.Count);
+            }
+    }
+}
