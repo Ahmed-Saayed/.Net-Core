dce4911f4f21f8c83e384f94db95c0c7639b122f
PATO <ahmed9pato7@gmail.com>
2025-01-09 02:17:09 +0200

    DONE

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+ 
+namespace Test                      // LeetCode Daily Challenges
+{
+    public class Solution
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int ans = 0;
+            var x = words;
+
+            Console.WriteLine(x[0]);
+            foreach (var s in words)
+            {
+                if (s.Length >= pref.Length && s.Substring(0, pref.Length) == pref)
+                    ans++;
+            }
+
+            return ans;
+        }
+    }
+    internal class Program { 
+        static void Main(string[] args) {
+            
+        }
+    }
+}
