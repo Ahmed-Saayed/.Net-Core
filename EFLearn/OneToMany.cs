c76c106cf0d41afd235e85a4a8328ee58bbb6e38
PATO <ahmed9pato7@gmail.com>
2025-01-18 00:21:51 +0200

    DONE

---
+++ EFLearn/OneToMany.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLearn
+{
+    internal class OneToMany
+    {
+    }
+
+    public class Car        // Principal table
+    {
+        public int CarID { get; set; }
+        public string CarName { get; set; }                 // principal key
+
+        public List<CarSale> CarSale { get; set; }          // collection navigation property
+    }
+    public class CarSale    // Dependent table
+    {
+        public int SaleID { get; set; }
+        public string SaleName { get; set; }
+
+        public string CN { get; set; }                      // foreign key
+
+        //[ForeignKey("CN")]
+        public Car Carr { get; set; }       // reference navigation property
+    }
+}
