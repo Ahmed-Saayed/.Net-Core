70a25f822c277437e3653724e1504d719a2e2897
Ahmed Sayed <ahmed9pato7@gmail.com>
2025-01-18 22:50:20 +0200

    DONE

--- EFLearn/DataCon.cs
+++ EFLearn/DataCon.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema ;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
